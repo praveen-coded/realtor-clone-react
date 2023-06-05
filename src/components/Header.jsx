eceed09d369e855f1c8a0ca33806f4f114b7cc6b
praveen-coded <developerpraveen11@gmail.com>
2023-06-05 15:41:53 +0530

    complete the header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default function Header() {
+    const [pageState, setPageState] = useState('Sign in');
     const location = useLocation();
     const navigate = useNavigate();
-    function pathMathRoute(route) {
+    const auth = getAuth();
+    useEffect(() => {
+        onAuthStateChanged(
+            auth,
+            (user) => {
+                if (user) {
+                    setPageState('Profile');
+                } else {
+                    setPageState('Sign in');
+                }
+            },
+            [auth]
+        );
+    });
+    function pathMatchRoute(route) {
         if (route === location.pathname) {
             return true;
         }
@@ -23,7 +40,7 @@ export default function Header() {
                     <ul className="flex space-x-10">
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                                pathMathRoute('/') &&
+                                pathMatchRoute('/') &&
                                 'text-black border-b-red-500'
                             }`}
                             onClick={() => navigate('/')}>
@@ -31,7 +48,7 @@ export default function Header() {
                         </li>
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                                pathMathRoute('/offers') &&
+                                pathMatchRoute('/offers') &&
                                 'text-black border-b-red-500'
                             }`}
                             onClick={() => navigate('/offers')}>
@@ -39,11 +56,12 @@ export default function Header() {
                         </li>
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                                pathMathRoute('/sign-in') &&
+                                (pathMatchRoute('/sign-in') ||
+                                    pathMatchRoute('/profile')) &&
                                 ' text-black  border-b-red-500'
                             }`}
-                            onClick={() => navigate('/sign-in')}>
-                            Sign in
+                            onClick={() => navigate('/profile')}>
+                            {pageState}
                         </li>
                     </ul>
                 </div>
