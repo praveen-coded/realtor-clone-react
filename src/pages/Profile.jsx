4d4697e7e937173eb916f270fc96ce637bb36da4
praveen-coded <developerpraveen11@gmail.com>
2023-06-15 19:26:50 +0530

    add delete & edit functionality to the my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,6 +10,7 @@ import {
     query,
     where,
     getDocs,
+    deleteDoc,
 } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import { FcHome } from 'react-icons/fc';
@@ -76,6 +77,19 @@ export default function Profile() {
         }
         fetchUserListings();
     }, [auth.currentUser.uid]);
+    async function onDelete(listingID) {
+        if (window.confirm('Are you sure you want to delete?')) {
+            await deleteDoc(doc(db, 'listings', listingID));
+            const updatedListings = listings.filter(
+                (listing) => listing.id !== listingID
+            );
+            setListings(updatedListings);
+            toast.success('Successfully deleted the listing');
+        }
+    }
+    function onEdit(listingID) {
+        navigate(`/edit-listing/${listingID}`);
+    }
     return (
         <>
             <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -143,12 +157,14 @@ export default function Profile() {
                         <h2 className="text-2xl text-center font-semibold mb-6">
                             My Listings
                         </h2>
-                        <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 t-6 mb-6">
+                        <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 t-6 mb-6">
                             {listings.map((listing) => (
                                 <ListingItem
                                     key={listing.id}
                                     id={listing.id}
                                     listing={listing.data}
+                                    onDelete={() => onDelete(listing.id)}
+                                    onEdit={() => onEdit(listing.id)}
                                 />
                             ))}
                         </ul>
